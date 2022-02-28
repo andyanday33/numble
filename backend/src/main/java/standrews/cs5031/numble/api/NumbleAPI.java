a2f0dadc3e4c3d19a3cef98454cd74492f4f8838
damonyu <damonyu97@hotmail.com>
2022-02-28 19:41:30 +0000

    initialise project

---
+++ backend/src/main/java/standrews/cs5031/numble/api/NumbleAPI.java
@@ -0,0 +1,49 @@
+package standrews.cs5031.numble.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import standrews.cs5031.numble.NumbleModel;
+import standrews.cs5031.numble.impl.NumbleModelImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin
+@RestController
+public class NumbleAPI {
+
+    private int gameId = 0;
+    private final Map<Integer, NumbleModel> games = new HashMap<>();
+
+    @PostMapping("/game")
+    public Integer startGame(
+            @RequestParam(name = "cols", defaultValue = "5") int cols,
+            @RequestParam(name = "rows", defaultValue = "5") int rows,
+            @RequestParam(name = "rhs", defaultValue = "16") int rhs,
+            @RequestParam(name = "lhs", defaultValue = "4+4*2") String lhs
+    ) {
+        NumbleModel model = new NumbleModelImpl(cols, rows, rhs, lhs);
+        games.put(++gameId, model);
+        return gameId;
+    }
+
+    @GetMapping("/game/{id}")
+    public NumbleModel getGame(@PathVariable int id) {
+        NumbleModel model = games.get(id);
+        if (model != null) {
+            return model;
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "game not found");
+    }
+
+    @PostMapping("/game/{id}/guess")
+    public NumbleModel makeGuess(@PathVariable int id, @RequestParam(name = "guess") String guess) {
+        NumbleModel model = games.get(id);
+        if (model != null) {
+            model.guess(guess);
+            return model;
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "game not found");
+    }
+}
