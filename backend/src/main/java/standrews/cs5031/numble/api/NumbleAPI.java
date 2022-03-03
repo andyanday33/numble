797b445484df877d56250f204be1471bbcd5eeb3
damonyu <damonyu97@hotmail.com>
2022-03-03 01:54:46 +0000

    start game with mode info, request rhs value when easy mode

--- backend/src/main/java/standrews/cs5031/numble/api/NumbleAPI.java
+++ backend/src/main/java/standrews/cs5031/numble/api/NumbleAPI.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import standrews.cs5031.numble.NumbleModel;
-import standrews.cs5031.numble.impl.NumbleModelImpl;
+import standrews.cs5031.numble.data.Config;
+import standrews.cs5031.numble.impl.NumbleModelEasyModeImpl;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,15 +16,19 @@ public class NumbleAPI {
 
     private int gameId = 0;
     private final Map<Integer, NumbleModel> games = new HashMap<>();
-
+    
     @PostMapping("/game")
-    public Integer startGame(
-            @RequestParam(name = "cols", defaultValue = "5") int cols,
-            @RequestParam(name = "rows", defaultValue = "5") int rows,
-            @RequestParam(name = "rhs", defaultValue = "16") int rhs,
-            @RequestParam(name = "lhs", defaultValue = "4+4*2") String lhs
-    ) {
-        NumbleModel model = new NumbleModelImpl(cols, rows, rhs, lhs);
+    public Integer startGame(@RequestParam("mode") NumbleModel.Mode mode) {
+        NumbleModel model = null;
+        if (mode == NumbleModel.Mode.EASY) {
+            //randomly select an equation from data source
+            //parse this equation to get rhs and lhs
+            int rhs = 6;
+            String lhs = "1+2+3";
+            model = new NumbleModelEasyModeImpl(Config.EASY_MODE_NUM_OF_ROWS, Config.EASY_MODE_NUM_OF_COLS, rhs, lhs);
+        } else {
+            //TODO create a hard mode game instance
+        }
         games.put(++gameId, model);
         return gameId;
     }
@@ -37,12 +42,28 @@ public class NumbleAPI {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "game not found");
     }
 
+    @GetMapping("/game/{id}/rhs")
+    public int getRhs(@PathVariable int id) {
+        NumbleModel model = games.get(id);
+        if (model != null) {
+            if (model instanceof NumbleModelEasyModeImpl) {
+                return ((NumbleModelEasyModeImpl) model).getRhs();
+            }
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "not allow to get right hand side value");
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "game not found");
+    }
+
     @PostMapping("/game/{id}/guess")
     public NumbleModel makeGuess(@PathVariable int id, @RequestParam(name = "guess") String guess) {
         NumbleModel model = games.get(id);
         if (model != null) {
-            model.guess(guess);
-            return model;
+            try {
+                model.guess(guess);
+                return model;
+            } catch (IllegalArgumentException e) {
+                return null;
+            }
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "game not found");
     }
