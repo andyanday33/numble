bde4c11f745d1c5ec2902dec4dbf201a9a061034
damonyu <damonyu97@hotmail.com>
2022-03-03 01:53:11 +0000

    stores game setup data

---
+++ backend/src/main/java/standrews/cs5031/numble/data/Config.java
@@ -0,0 +1,11 @@
+package standrews.cs5031.numble.data;
+
+/**
+ * This stores configuration data for game setup.
+ */
+public class Config {
+    public static final int EASY_MODE_NUM_OF_ROWS = 5;
+    public static final int EASY_MODE_NUM_OF_COLS = 5;
+    public static final int HARD_MODE_NUM_OF_ROWS = 8;
+    public static final int HARD_MODE_NUM_OF_COLS = 6;
+}
