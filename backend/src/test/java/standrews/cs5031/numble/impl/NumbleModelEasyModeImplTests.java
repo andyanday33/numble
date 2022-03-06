b4bb47263acb92cc3949803c7a9ebe44ba74f745
ConorJEM <mcgale98@gmail.com>
2022-03-06 10:18:37 +0000

    Fixed Mockito issue with testing

--- backend/src/test/java/standrews/cs5031/numble/impl/NumbleModelEasyModeImplTests.java
+++ backend/src/test/java/standrews/cs5031/numble/impl/NumbleModelEasyModeImplTests.java
@@ -1,6 +1,7 @@
 package standrews.cs5031.numble.impl;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
@@ -10,19 +11,30 @@ import standrews.cs5031.numble.NumbleModel;
 import standrews.cs5031.numble.data.EquationData;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 public class NumbleModelEasyModeImplTests {
     NumbleModelEasyModeImpl model;
+    static MockedStatic<EquationData> equationData;
 
-    @BeforeEach
-    public void setup() {
+    @BeforeAll
+    public static void setup() {
         MockedStatic<EquationData> equationData = Mockito.mockStatic(EquationData.class);
         equationData.when(() -> EquationData.getRandomEquation(NumbleModel.Mode.EASY, 3))
                 .thenReturn("3+2=5");
-        model = new NumbleModelEasyModeImpl(2, 3);
+    }
+
+    @AfterAll
+    public static void close(){
+        try {
+            equationData.close();
+        } catch(NullPointerException e) {
+        }
     }
 
     @Test
     public void gameCreated() {
+        model = new NumbleModelEasyModeImpl(2, 3);
         assertEquals(NumbleModel.Mode.EASY, model.getMode());
         assertEquals(2, model.getNumRows());
         assertEquals(3, model.getNumCols());
@@ -32,10 +44,14 @@ public class NumbleModelEasyModeImplTests {
             }
         }
         assertEquals(5, model.getRhs());
+
+        //Try another guess
+        assertThrows(MethodNotAvailableException.class, () -> model.guess("1+2"));
     }
 
     @Test
     public void noGuessAfterPlayerWins() {
+        model = new NumbleModelEasyModeImpl(2, 3);
         //Win the game
         String correctSolution = "3+2";
         assertTrue(model.guess(correctSolution));
@@ -47,6 +63,7 @@ public class NumbleModelEasyModeImplTests {
 
     @Test
     public void noGuessAfterPlayerLoses() {
+        model = new NumbleModelEasyModeImpl(2, 3);
         //Lose the game
         String wrongSolution = "3+1";
         assertFalse(model.guess(wrongSolution));
@@ -60,6 +77,7 @@ public class NumbleModelEasyModeImplTests {
 
     @Test
     public void guessWithInvalidInputs() {
+        model = new NumbleModelEasyModeImpl(2, 3);
         String[] invalidInputs = {"", "1", "+", "1234", "1+2+3",
                                     "1++", "+1-", "1A2", "1/2", "--5",
                                     "1+2", "9*1", "4-2", "123"};
